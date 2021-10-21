a3aa02ae0a6bc44829d0e8e1fff86195c4b6c8a7
Gabriel Delolmo <delanoerhardt@gmail.com>
2021-10-21 14:15:08 -0300

    Fixed the links on the navbar I hate CSS

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -9,17 +9,48 @@ const Nav = styled.nav`
   justify-content: space-between;
 `;
 
+const Img = styled.img`
+  height: 100%;
+  margin-left: 5px;
+  margin-top: 5px;
+`;
+
+const NavLinksDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+
+const LinkDiv = styled.div`
+  height: 100%;
+  width: 5rem;
+  display: flex;
+  align-items: center;
+`;
+
+const LinkStyled = styled(Link)`
+  height: 100%;
+  width: 5rem;
+  text-decorations: none;
+  text-align: center;
+`;
+
 function Navbar(props) {
   const mainLink = props.mainLink || links.main;
 
   return (
     <Nav>
       <Link to={mainLink.path}>
-        <h1>Slá</h1>
+        <Img src="/pokedex_logo.png"></Img>
       </Link>
-      {props.links?.map((el) => (
-        <Link to={el.path}>{el.name}</Link>
-      ))}
+      <NavLinksDiv>
+        {props.links?.map((el) => (
+          <LinkDiv>
+            <LinkStyled to={el.path}>{el.name}</LinkStyled>
+          </LinkDiv>
+        ))}
+      </NavLinksDiv>
     </Nav>
   );
 }
